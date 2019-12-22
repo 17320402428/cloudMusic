0905ff731cb1e8f0ecf94a72fd37c9e43cf98b64
haimianbaobao <1940107928@qq.com>
2019-12-22 15:28:50 +0800

    初始化

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,42 +1,33 @@
+<!--  -->
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">{{title}}</text>
-		</view>
-	</view>
+  <div class="">
+    <van-button type="info">信息按钮</van-button>
+  </div>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
+//这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
+//例如：import 《组件名称》 from '《组件路径》';
+import VanButton from "@/wxcomponents/dist/button/index"
+export default {
+  //import引入的组件需要注入到对象中才能使用
+  components: {
+    VanButton
+  },
+  data() {
+    //这里存放数据
+    return {}
+  },
+  //监听属性 类似于data概念
+  computed: {},
+  //监控data中的数据变化
+  watch: {},
+  //方法集合
+  methods: {},
+  //生命周期 - 创建完成（可以访问当前this实例）
+  created() {}
+}
 </script>
+<style lang="less" scoped>
 
-<style>
-	.content {
-		text-align: center;
-		height: 400upx;
-	}
-
-	.logo {
-		height: 200upx;
-		width: 200upx;
-		margin-top: 200upx;
-	}
-
-	.title {
-		font-size: 36upx;
-		color: #8f8f94;
-	}
 </style>
